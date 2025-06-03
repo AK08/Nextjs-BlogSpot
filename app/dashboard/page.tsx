d805f2c50367bcfebd3a42d08a5061097668b513
Alen Kottaram <alen.k@varuns2017outlook.onmicrosoft.com>
2025-06-03 14:48:07 +0530

    using dynamic rendering

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,11 @@
+export const dynamic = 'force-dynamic';
+
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 import { prisma } from "../utils/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { BlogPostCard } from "@/components/general/BlogpostCard";
-import type {IappProps} from "@/components/general/BlogpostCard";
+import type { IappProps } from "@/components/general/BlogpostCard";
 
 async function getData(userId: string) {
   await new Promise((resolve) => setTimeout(resolve, 2000));
@@ -31,7 +33,7 @@ export default async function DashboardRoute() {
   return (
     <div>
       <div className="flex items-center justify-between mb-4">
-        <h2 className="text-xl font-medium"> Your Blog Articles</h2>
+        <h2 className="text-xl font-medium">Your Blog Articles</h2>
         <Link className={buttonVariants()} href="/dashboard/create">
           Create Post
         </Link>
